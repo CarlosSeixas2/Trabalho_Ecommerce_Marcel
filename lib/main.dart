e215cfb03da038da299235eadda83a1f2f7320a0
Houston Barros <capar.2022113tads0170@aluno.ifpi.edu.br>
2024-04-03 22:45:15 -0300

    feat(cards): added cards style

--- lib/main.dart
+++ lib/main.dart
@@ -38,108 +38,17 @@ class _MyAppState extends State<MyApp> {
       "quantidade": 20
     },
     {
-      "nome": "Placa de Vídeo",
+      "nome": "Placa de Vídeo RTX 4050",
       "descricao": "Placa de vídeo NVIDIA GeForce RTX 3060",
       "preco": 499.99,
       "quantidade": 10
-    },
-    {
-      "nome": "HD Externo",
-      "descricao": "HD Externo USB 3.0 1TB",
-      "preco": 79.99,
-      "quantidade": 25
-    },
-    {
-      "nome": "Roteador Wi-Fi",
-      "descricao": "Roteador Wi-Fi dual-band AC1200",
-      "preco": 49.99,
-      "quantidade": 15
-    },
-    {
-      "nome": "Impressora",
-      "descricao": "Impressora multifuncional a jato de tinta",
-      "preco": 129.99,
-      "quantidade": 35
-    },
-    {
-      "nome": "Cabo HDMI",
-      "descricao": "Cabo HDMI 2.0 de alta velocidade 2 metros",
-      "preco": 9.99,
-      "quantidade": 5
-    },
-    {
-      "nome": "Adaptador USB",
-      "descricao": "Adaptador USB-C para HDMI, USB-A e Ethernet",
-      "preco": 29.99,
-      "quantidade": 8
-    },{
-      "nome": "Notebook",
-      "descricao": "Notebook ultrafino com SSD",
-      "preco": 899.99,
-      "quantidade": 5
-    },
-    {
-      "nome": "Mouse",
-      "descricao": "Mouse óptico sem fio",
-      "preco": 19.99,
-      "quantidade": 50
-    },
-    {
-      "nome": "Teclado",
-      "descricao": "Teclado mecânico RGB",
-      "preco": 59.99,
-      "quantidade": 30
-    },
-    {
-      "nome": "Monitor",
-      "descricao": "Monitor LED 24 polegadas",
-      "preco": 199.99,
-      "quantidade": 20
-    },
-    {
-      "nome": "Placa de Vídeo",
-      "descricao": "Placa de vídeo NVIDIA GeForce RTX 3060",
-      "preco": 499.99,
-      "quantidade": 10
-    },
-    {
-      "nome": "HD Externo",
-      "descricao": "HD Externo USB 3.0 1TB",
-      "preco": 79.99,
-      "quantidade": 25
-    },
-    {
-      "nome": "Roteador Wi-Fi",
-      "descricao": "Roteador Wi-Fi dual-band AC1200",
-      "preco": 49.99,
-      "quantidade": 15
-    },
-    {
-      "nome": "Impressora",
-      "descricao": "Impressora multifuncional a jato de tinta",
-      "preco": 129.99,
-      "quantidade": 35
-    },
-    {
-      "nome": "Cabo HDMI",
-      "descricao": "Cabo HDMI 2.0 de alta velocidade 2 metros",
-      "preco": 9.99,
-      "quantidade": 5
-    },
-    {
-      "nome": "Adaptador USB",
-      "descricao": "Adaptador USB-C para HDMI, USB-A e Ethernet",
-      "preco": 29.99,
-      "quantidade": 8
-    },
+    }
   ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Sora'
-      ),
+      theme: ThemeData(fontFamily: 'Sora'),
       home: Scaffold(
         backgroundColor: const Color(0xff09090B),
         appBar: AppBar(
@@ -151,18 +60,13 @@ class _MyAppState extends State<MyApp> {
           ),
           centerTitle: true,
           iconTheme: const IconThemeData(
-              color: Color(0xffFAFAFA),
+            color: Color(0xffFAFAFA),
           ),
           backgroundColor: Colors.transparent,
-
           flexibleSpace: Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
-                colors: [
-                  Color(0xff09090B),
-                  // Colors.purple,
-                  Color(0xff001008)
-                ],
+                colors: [Color(0xff09090B), Color(0xff001008)],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
@@ -181,7 +85,6 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
           ),
-
           actions: [
             IconButton(
               onPressed: () {
@@ -209,21 +112,25 @@ class _MyAppState extends State<MyApp> {
               ),
             ],
           ),
-
         ),
         body: SingleChildScrollView(
           child: Center(
             child: Column(
               children: [
                 const SizedBox(
-                  height: 15,
+                  height: 30,
                 ),
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.all(15),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
                   child: Row(
                     children: [
-                      for (var categoria in ['Periféricos', 'Espaço Gamer', 'Hardware', 'Computadores'])
+                      for (var categoria in [
+                        'Periféricos',
+                        'Espaço Gamer',
+                        'Hardware',
+                        'Computadores'
+                      ])
                         Row(
                           children: [
                             CategoryCard(nome: categoria),
@@ -236,31 +143,105 @@ class _MyAppState extends State<MyApp> {
                 const SizedBox(
                   height: 30,
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Text(
-                      'Recomendados',
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Ver todas',
-                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                    ),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Recomendados',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xffFAFAFA),
+                        ),
+                      ),
+                      Text(
+                        'Ver todas',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff01FC80),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
                 const SizedBox(
                   height: 15,
                 ),
-                Wrap(
-                  alignment: WrapAlignment.start,
-                  children: [
-                    for (var produto in produtos)
-                      ProductCard(
-                        nome: produto['nome'],
-                        preco: produto['preco'],
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (var produto in produtos)
+                        Row(
+                          children: [
+                            ProductCard(
+                              nome: produto['nome'],
+                              preco: produto['preco'],
+                            ),
+                            const SizedBox(width: 25),
+                          ],
+                        )
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 30,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Periféricos',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xffFAFAFA),
+                        ),
                       ),
-                  ],
+                      Text(
+                        'Ver todas',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff01FC80),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      for (var produto in produtos)
+                        Row(
+                          children: [
+                            ProductCard(
+                              nome: produto['nome'],
+                              preco: produto['preco'],
+                            ),
+                            const SizedBox(width: 25),
+                          ],
+                        )
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -271,8 +252,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
-
 class ProductCard extends StatelessWidget {
   const ProductCard({super.key, required this.nome, required this.preco});
 
@@ -282,21 +261,70 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 115,
-      height: 180,
+      width: 140,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            width: 70,
-            height: 115,
-            child: Image.asset(
-              'assets/images/notebook.jpg',
-              fit: BoxFit.cover,
+            width: 140,
+            height: 140,
+            child: Container(
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/notebook.jpg'),
+                  fit: BoxFit.cover,
+                ),
+                border: Border.all(
+                  color: const Color(0xff27272A),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
           ),
-          Text(nome),
-          Text('R\$ ${preco.toStringAsFixed(2)}'),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 5,
+              ),
+
+              const Text(
+                "Apenas 5 unidades",
+                style: TextStyle(
+                  color: Color(0xffA1A1AA),
+                  fontSize: 8,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+
+              const SizedBox(
+                height: 5,
+              ),
+
+              Text(
+                nome,
+                style: const TextStyle(
+                  color: Color(0xffFAFAFA),
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              
+              const SizedBox(
+                height: 5,
+              ),
+
+              Text(
+                'R\$ ${preco.toStringAsFixed(2).replaceAll('.', ',')}',
+                style: const TextStyle(
+                  color: Color(0xff01FC80),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
@@ -332,7 +360,6 @@ class CategoryCard extends StatelessWidget {
         child: Center(
           child: Text(
             nome,
-
             style: const TextStyle(
               color: Color(0xffFAFAFA),
               fontSize: 16,
